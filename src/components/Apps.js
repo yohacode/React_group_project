b912d097183ca89f21b219badfd30929eea4587f
yohacode <johnpro3269@gmail.com>
2025-05-28 20:22:53 +0300

    Testing React With Team.

---
+++ src/components/Apps.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+      </header>
+      <main>
+        <p>This is a simple React application.</p>
+      </main>
+    </div>
+  );
+}
+export default App;
